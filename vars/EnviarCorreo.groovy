b6e7bbc89ba5b82ec40ff88b1373aff791f04f29
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2025-06-26 02:17:47 -0500

    Email changed

--- vars/EnviarCorreo.groovy
+++ vars/EnviarCorreo.groovy
@@ -32,7 +32,7 @@ def call(Map params) {
     """
 
     emailext(
-        to: "$DEFAULT_RECIPIENTS",
+        to: "juanfernandoam@hotmail.com",
         subject: "Imagen Docker actualizada - ${repositorio}",
         body: mensaje,
         mimeType: 'text/html'
