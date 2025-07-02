1671571ef2a1421cb444bd8ead378d7b0511ce40
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2025-07-02 04:35:08 -0500

    replace spaces with _ name log

--- vars/EjecutarEtapa.groovy
+++ vars/EjecutarEtapa.groovy
@@ -5,7 +5,7 @@ def call(String nombreEtapa = null, String buildNumber = null, Closure bloque) {
         bloque()
     } catch (Exception e) {
         def mensajeError = "Error en etapa ${etapa}:\n${e.getMessage()}"
-        env.LOG_FILE = "error-${etapa}-${build}.log"
+        env.LOG_FILE = "error-${etapa.replace(' ', '_')}-${build}.log"
         writeFile file: "${env.LOG_FILE}", text: mensajeError
         currentBuild.result = 'FAILURE'
         error("Falló la etapa ${etapa}")
