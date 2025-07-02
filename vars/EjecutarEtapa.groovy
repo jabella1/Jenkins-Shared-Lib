0a29600362fcec9e72849400f857cf064dcb091a
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2025-07-02 04:19:42 -0500

    Fix : mejoraa nombre archivo

--- vars/EjecutarEtapa.groovy
+++ vars/EjecutarEtapa.groovy
@@ -1,10 +1,12 @@
-def call(String nombreEtapa = null, Closure bloque) {
+def call(String nombreEtapa = null, String buildNumber = null, Closure bloque) {
     def etapa = nombreEtapa ?: (env.STAGE_NAME ?: "etapa_desconocida")
+    def build = buildNumber ?: (env.BUILD_NUMBER ?: "build_desconocido")
     try {
         bloque()
     } catch (Exception e) {
         def mensajeError = "Error en etapa ${etapa}:\n${e.getMessage()}"
-        writeFile file: "${env.LOG_FILE}", text: mensajeError
+        env.LOG_FILE = "error-${etapa}-${build}.log"
+        writeFile file: "${env.LOG_FILE}", text: mensajeError, overwrite: true
         currentBuild.result = 'FAILURE'
         error("Falló la etapa ${etapa}")
     }
